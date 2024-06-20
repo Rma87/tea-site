055f433ae993f07e0087d7ef91384141c75bfc64
Rastin Rostami zadeh <rrastin633@gmail.com>
2024-06-20 13:24:54 +0330

    complete
    complete a project

--- tea/urls.py
+++ tea/urls.py
@@ -1,13 +1,11 @@
 from django.urls import path
 from tea.views import *
 
-app_name = "tea"
+app_name = "website"
 
 urlpatterns = [
-    path('', home_page, name='index'),
-    path('about/', about_page, name='about'),
-    path('contact', contact_page, name='contact'),
-    path('store/', store_page, name='store'),
-    
-    
+    path('',home_page, name='index'),
+    path('about/',about_page, name='about'),
+    path('contact/',contact_page, name='contact'),
+    path('store/',store_page, name='store'),
 ]
